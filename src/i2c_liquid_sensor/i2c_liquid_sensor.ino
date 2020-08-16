218d49b465a2b11b4e6d56b773772445ee6c7847
Pasquale <puskyer@gmail.com>
2020-08-16 18:42:26 -0400

    latest attempt to use Onion Omega and Arduino Dock r1 for liquid sensor

--- src/i2c_liquid_sensor/i2c_liquid_sensor.ino
+++ src/i2c_liquid_sensor/i2c_liquid_sensor.ino
@@ -12,23 +12,28 @@
 * All above must be included in any redistribution
 * ****************************************************/
 #include <Arduino.h>
+#include <avr/wdt.h>
 #include <Wire.h>
 
+// I2C slave address for arduino
+#define SLAVE_ADDRESS 0x08
 // Pin 13 has an LED connected on most Arduino boards.
 // give it a name:
 int led0 = 13;
-int liqSpin = 5;
-#define SLAVE_ADDRESS 0x08
+
+int liqSHin = 5;
+byte Liquid_High_level=0;
+int liqSLin = 6;
+byte Liquid_Low_level=0;
+
 byte data_send[2] = {0,0};
-byte command_recived = 0;
 byte data_recived[2] = {0,0};
+byte command_recived = 0;
 byte data_recived_count = 0;
 
-int Liquid_level=0;
+
 
 void setup() {
-
-    Wire.begin(); // Wire communication begin
 
     Serial.begin(115200);  // start serial
     while(!Serial){} // Waiting for serial connection
@@ -37,16 +42,16 @@ void setup() {
     Serial.println();
     Serial.println("Start I2C comm");
       // initialize the Liquid Sensor pin as an input.
-    pinMode(liqSpin,INPUT);
+    pinMode(liqSin,INPUT);
     Serial.print("Liquid Sensor pin is ");
-    Serial.println(liqSpin);
+    Serial.println(liqSin);
     
       // initialize the led pin as an output.
     pinMode(led0, OUTPUT);  
     Serial.println("Led is enabled ");
     
+    // Wire communication begin
     Wire.begin(SLAVE_ADDRESS);
-
     Wire.onReceive(receiveData);
     Wire.onRequest(sendData);
 
@@ -56,34 +61,42 @@ void setup() {
 void loop() {
 
 
-//  Liquid_level=digitalRead(5);
-  Liquid_level = 5;
-  data_send[1] = Liquid_level;
+  Liquid_High_level=digitalRead(liqSHin);
+  Liquid_Low_level=digitalRead(liqSLin);
+
+
+  if ( Liquid_High_level == 0 ) {
+
   
+  data_send[0] = Liquid_level;
+
     switch (data_recived[0]) {
         case 1:
         //do something when data_recived is 1
+          Serial.print("data_recived_count  ");
+          Serial.println(data_recived_count, HEX);
           Serial.print("Recived command ");
-          Serial.println(data_recived[0], DEC);
-          Serial.print("data  ");
-          Serial.println(data_recived[1], DEC);
+          Serial.println(data_recived[0], HEX);
           digitalWrite(led0, HIGH);   // turn the LED on (HIGH is the voltage level)
           Serial.println("Led turned on!");
-          sendData();
+          Serial.print("Liquid level is ");
+          Serial.println(data_send[0],HEX);
           data_recived[0] = 0;
           break;
         case 2:
         //do something when data_recived is 2
-          Serial.println(data_recived[0], DEC);
-          Serial.print("data  ");
-          Serial.println(data_recived[1], DEC);
+          Serial.print("Recived command ");
+          Serial.print(data_recived[0], HEX);
           digitalWrite(led0, LOW);    // turn the LED off by making the voltage LOW
+          Serial.println(" Led turned off!");
           Serial.print("Liquid level is ");
-          Serial.println(data_send[1],DEC);
+          Serial.println(data_send[0],DEC);
           data_recived[0] = 0;
           break;
         default:
           // if nothing else matches, do the default
+ //         Serial.print("Liquid level is ");
+ //         Serial.println(data_send[1],DEC);
           // default is optional
           break;
       }
@@ -92,17 +105,58 @@ void loop() {
 
 }
 
-void receiveData(int bytecount)
-{
+void receiveData(int bytecount){
   for (int i = 0; i < bytecount; i++) {
-    data_recived_count = bytecount;
     data_recived[i] = Wire.read();
-  }  
+  }
+  // the reset 
+  if ( data_recived[0] == 0xde && data_recived[1] == 0xad) {
+     _SoftwareReset();
+     Serial.println("Reset!");
+     } else {
+     data_recived_count = bytecount;      
+     }
 }
 
-void sendData()
-{
-  for (int i=0; i < data_recived_count; i++) {
-     Wire.write(data_send[i]);
+void sendData(){
+//    if (Wire.available() > 0){
+      for (int i=0; i < data_recived_count; i++){
+        Serial.print("data_send ");
+        Serial.println(data_send[i],HEX);
+        Wire.write(data_send[i]);
+      }
+      data_recived_count = 1;
+//    }
+}
+
+// Restarts program from beginning but does not reset the peripherals and registers
+void _SoftwareReset() {
+  wdt_enable(WDTO_15MS);
+} 
+
+
+// function that executes whenever data is received from master
+void receiveEvent(int arg) {
+  int addr;
+  int data;
+  data_recived_count = 2;
+  // receive bytes from i2c bus
+  while (Wire.available() > 0) {
+    // read the register address being written to
+    addr  = Wire.read();
+
+    // handle received commands and data
+    if (Wire.available() > 0) {
+      data = Wire.read();
+
+      // the reset 
+      if (addr == 0xde && data == 0xad) {
+        _SoftwareReset();
+        Serial.println("Reset!");
+      } else {
+          data_recived[0] = addr;
+          data_recived[1] = data;
+        }
+    }
   }
 }
